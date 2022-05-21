a2c897745ec68c12d147737af17c73f10b6182e2
hyungseok kim <witbring@kaist.ac.kr>
2022-05-21 11:12:35 +0900

    update ddisasm

--- tools/ddisasm.py
+++ tools/ddisasm.py
@@ -17,11 +17,14 @@ def ddisasm_cmd(conf):
 
 class Ddisasm(Manager):
     def __init__(self, core, bench='/data3/1_reassessor/benchmark', tool_path='/data3/1_reassessor/new_ddisasm2', tool_name='ddisasm'):
+        self.overwrite = False
         super().__init__(core, bench, tool_path, tool_name)
 
     def gen_option(self, work_dir, tool_name):
         ret = []
-        for pack in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
+        #for pack in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
+        #for pack in ['coreutils-8.30', 'binutils-2.31.1']:
+        for pack in ['spec_cpu2006']:
             for arch in ['x64', 'x86']:
                 #for comp in ['gcc' ]:
                 for comp in ['clang', 'gcc' ]:
@@ -32,6 +35,13 @@ class Ddisasm(Manager):
                                 sub_dir = '%s/%s/%s/%s/%s-%s'%(pack, arch, comp, popt, opt, lopt)
                                 for binary in glob.glob('%s/%s/bin/*'%(work_dir, sub_dir)):
                                     filename = os.path.basename(binary)
+
+                                    if not self.overwrite:
+                                        output = '%s/%s/%s/%s.s'%(self.tool_path, sub_dir, tool_name, filename)
+                                        if os.path.isfile(output):
+                                            continue
+                                        #print(output)
+
 
                                     ret.append(REConf(self.bench, tool_name, self.tool_path, sub_dir, filename, arch, comp, popt, lopt, ddisasm_cmd))
         return ret
@@ -44,7 +54,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     mgr = Ddisasm(args.core)
-    mgr.print_conf()
-    #mgr.run()
-    mgr.print_compile_cmd()
+    #mgr.print_conf()
+    mgr.run()
+    #mgr.print_compile_cmd()
     #mgr.compile()
