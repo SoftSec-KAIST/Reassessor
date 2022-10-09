f7664fdfd1e9cd66df5c1ba205e04e14e83597cb
Hyungseok Kim <witbring@kaist.ac.kr>
2022-10-09 16:07:00 +0900

    add preprocessing script

---
+++ demos/run_preproc.py
@@ -0,0 +1,118 @@
+from collections import namedtuple
+import glob, os
+import multiprocessing
+
+BuildConf = namedtuple('BuildConf', ['target', 'input_root', 'sub_dir', 'output_dir', 'arch', 'pie', 'package'])
+
+def single_run(target):
+    input_root = './dataset'
+    output_root = './output'
+    package, arch, _, popt, _ = target.split('/')[-7:-2]
+    sub_dir = '/'.join(target.split('/')[-7:-2])
+
+    filename = os.path.basename(target)
+    output_dir = '%s/%s/%s'%(output_root, sub_dir, filename)
+    conf = BuildConf(target, input_root, sub_dir, output_dir, arch, popt, package)
+
+    job(conf)
+
+def gen_option(input_root, output_root, package):
+    ret = []
+    cnt = 0
+    for arch in ['x86', 'x64']:
+        for comp in ['clang', 'gcc']:
+            for popt in ['pie', 'nopie']:
+                for opt in ['o0', 'o1', 'o2', 'o3', 'os', 'ofast']:
+                    for lopt in ['bfd', 'gold']:
+                        sub_dir = '%s/%s/%s/%s/%s-%s'%(package, arch, comp, popt, opt, lopt)
+                        input_dir = '%s/%s'%(input_root, sub_dir)
+                        for target in glob.glob('%s/reloc/*'%(input_dir)):
+
+                            filename = os.path.basename(target)
+
+                            output_dir = '%s/%s/%s'%(output_root, sub_dir, filename)
+
+                            ret.append(BuildConf(target, input_root, sub_dir, output_dir, arch, popt, package))
+
+                            cnt += 1
+    return ret
+
+def job(conf):
+
+    ramblr_output = conf.output_dir+'/reassem/ramblr.s'
+    retrowrite_output = conf.output_dir+'/reassem/retrowrite.s'
+    ddisasm_output = conf.output_dir+'/reassem/ddisasm.s'
+
+    reassem_list = ['ramblr', 'retrowrite', 'ddisasm']
+    reassem_dict = dict()
+
+    for reassem in reassem_list:
+        reassem_dict[reassem] = True
+
+    if conf.pie != 'pie':
+        reassem_dict['retrowrite'] = False
+        if os.path.exists(ramblr_output):
+            reassem_dict['ramblr'] = False
+        if os.path.exists(ddisasm_output):
+            reassem_dict['ddisasm'] = False
+    else:
+        reassem_dict['ramblr'] = False
+        if conf.arch != 'x64':
+            reassem_dict['retrowrite'] = False
+        else:
+            if os.path.exists(retrowrite_output):
+                reassem_dict['retrowrite'] = False
+        if os.path.exists(ddisasm_output):
+            reassem_dict['ddisasm'] = False
+
+    options = ''
+    bRun = False
+    for reassem in reassem_list:
+        if not reassem_dict[reassem]:
+            options += ' --no-%s'%(reassem)
+        else:
+            print(reassem)
+            bRun = True
+
+    if bRun:
+        print('python3 ../reassessor/preprocessing.py %s %s %s'%(conf.target, conf.output_dir, options))
+
+    #from reassessor.preprocessing import Preprocessing
+    #preproc = Preprocessing(conf.target, conf.output_dir, arch=conf.arch, pie=conf.pie)
+    #preproc.run(reset=False, bDdisasm, bRamblr, bRetro)
+
+
+def run(package, core=1):
+    if package not in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
+        return False
+    input_root= './dataset'
+    output_dir = './output'
+    config_list = gen_option(input_root, output_dir, package)
+
+    if core and core > 1:
+        p = multiprocessing.Pool(core)
+        p.map(job, [(conf) for conf in config_list])
+    else:
+        for conf in config_list:
+            job(conf)
+
+
+import argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='manager')
+    parser.add_argument('--package', type=str, help='Package')
+    parser.add_argument('--core', type=int, default=1, help='Number of cores to use')
+    parser.add_argument('--target', type=str)
+
+    args = parser.parse_args()
+
+    if args.target:
+        single_run(args.target)
+    elif args.package:
+        run(args.package, args.core)
+    else:
+        #for package in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
+        for package in ['binutils-2.31.1']:
+            run(package, args.core)
+
+
