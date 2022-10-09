7ad025fa74cc81e5d551b1ec1a1e5538dabf0557
Hyungseok Kim <witbring@kaist.ac.kr>
2022-10-09 16:23:57 +0900

    separate preprocessing from demo script

--- demos/demo.py
+++ demos/demo.py
@@ -1,12 +1,10 @@
-from reassessor.reassessor import Reassessor
-from reassessor.preprocessing import Preprocessing
 from collections import namedtuple
 import glob, os
 import multiprocessing
 
 BuildConf = namedtuple('BuildConf', ['target', 'input_root', 'sub_dir', 'output_path', 'arch', 'pie', 'package'])
 
-def single_run(target):
+def single_run(target, bDocker=False):
     input_path = './dataset'
     output_path = './output'
     package, arch, _, popt, _ = target.split('/')[-7:-2]
@@ -16,7 +14,11 @@ def single_run(target):
     out_dir = '%s/%s/%s'%(output_path, sub_dir, filename)
     conf = BuildConf(target, input_path, sub_dir, out_dir, arch, popt, package)
 
-    job(conf)
+    if not bDocker:
+        job(conf)
+    else:
+        docker_job(conf)
+
 
 def gen_option(input_root, output_root, package):
     ret = []
@@ -39,12 +41,8 @@ def gen_option(input_root, output_root, package):
                             cnt += 1
     return ret
 
-def job(conf, bPreprocessing=True):
+def job(conf):
     reassem_dict = dict()
-
-    if bPreprocessing:
-        preproc = Preprocessing(conf.target, conf.output_path)
-        preproc.run(reset=False, bDdisasm=False, bRamblr=False, bRetro=False)
 
     ramblr_output = conf.output_path+'/reassem/ramblr.s'
     retrowrite_output = conf.output_path+'/reassem/retrowrite.s'
@@ -57,6 +55,8 @@ def job(conf, bPreprocessing=True):
     if os.path.exists(ddisasm_output):
         reassem_dict['ddisasm'] = ddisasm_output
 
+    from reassessor.reassessor import Reassessor
+
     if conf.package in ['spec_cpu2006']:
         reassessor = Reassessor(conf.target, '%s/%s/asm/%s'%(conf.input_root, conf.sub_dir, os.path.basename(conf.target)), conf.output_path, conf.input_root)
     else:
@@ -64,22 +64,22 @@ def job(conf, bPreprocessing=True):
 
     reassessor.run(reassem_dict)
 
-def docker_job(conf, bPreprocessing=True):
-    cmd = 'sudo docker run --rm -v %s:/input -v %s:/output reassessor sh -c "/Reassessor/reassesor.py /input/%s/reloc/%s /input/%s/assem /output/ /input/'%(conf.input_root, conf.output_path, conf.sub_dir, os.path.basename(conf.target), conf.sub_dir, os.path.basename(conf.target))
-    if bPreprocessing:
-        download()
-    else:
-        if os.path.exists(conf.output_path+'/reassem/ramblr.s'):
-            cmd += ' --ramblr /output/reassem/ramblr.s'
-        if os.path.exists(conf.output_path+'/reassem/retrowrite.s'):
-            cmd += ' --retrowrite /output/reassem/retrowrite.s'
-        if os.path.exists(conf.output_path+'/reassem/ddisasm.s'):
-            cmd += ' --ddisasm /output/reassem/ddisasm.s'
+def docker_job(conf):
+    filename=os.path.basename(conf.target)
+    cmd = 'sudo docker run --rm -v %s:/input -v %s:/output reassessor sh -c '%(conf.input_root, conf.output_path)
+    cmd += '"python3 /Reassessor/reassessor/reassesor.py /input/%s/reloc/%s /input/%s/asm /output/ /input/"'%(conf.sub_dir, filename, conf.sub_dir)
+    if os.path.exists(conf.output_path+'/reassem/ramblr.s'):
+        cmd += ' --ramblr /output/reassem/ramblr.s'
+    if os.path.exists(conf.output_path+'/reassem/retrowrite.s'):
+        cmd += ' --retrowrite /output/reassem/retrowrite.s'
+    if os.path.exists(conf.output_path+'/reassem/ddisasm.s'):
+        cmd += ' --ddisasm /output/reassem/ddisasm.s'
     cmd += '"'
-    os.system(cmd)
+    print(cmd)
+    #os.system(cmd)
 
 
-def run(package, core=1, bPreprocessing=True, bDocker=False):
+def run(package, core=1, bDocker=False):
     if package not in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
         return False
     input_path = './dataset'
@@ -89,15 +89,15 @@ def run(package, core=1, bPreprocessing=True, bDocker=False):
     if core and core > 1:
         p = multiprocessing.Pool(core)
         if not bDocker:
-            p.starmap(job, [(conf, bPreprocessing) for conf in config_list])
+            p.map(job, [(conf) for conf in config_list])
         else:
-            p.starmap(docker_job, [(conf, bPreprocessing) for conf in config_list])
+            p.map(docker_job, [(conf) for conf in config_list])
     else:
         for conf in config_list:
             if not bDocker:
-                job(conf, bPreprocessing)
+                job(conf)
             else:
-                docker_job(conf, bPreprocessing)
+                docker_job(conf)
 
 
 import argparse
@@ -105,18 +105,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='manager')
     parser.add_argument('--package', type=str, help='Package')
     parser.add_argument('--core', type=int, default=1, help='Number of cores to use')
-    parser.add_argument('--no-preprocessing', dest='preprocessing', action='store_false')
     parser.add_argument('--docker', action='store_true')
     parser.add_argument('--target', type=str)
 
     args = parser.parse_args()
 
     if args.target:
-        single_run(args.target)
+        single_run(args.target, args.docker)
     elif args.package:
-        run(args.package, args.core, args.preprocessing, args.docker)
+        run(args.package, args.core, args.docker)
     else:
         for package in ['coreutils-8.30', 'binutils-2.31.1', 'spec_cpu2006']:
-            run(package, args.core, args.preprocessing, args.docker)
-
-
+            run(package, args.core, args.docker)
