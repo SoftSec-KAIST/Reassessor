8cccf72f410d99c9e502f2b73b11922deabb714b
hyungseok kim <witbring@kaist.ac.kr>
2022-10-10 10:13:01 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.91',
     author='Hyungseok Kim',
     description='Reassessor',
-    packages=['reassessor', 'reassessor.lib', 'reassessor.normalizer', 'reassessor.differ', 'reassessor.misc'],
+    packages=['reassessor', 'reassessor.lib', 'reassessor.normalizer', 'reassessor.differ'],
 )
